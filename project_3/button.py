94c321ef096e8b04aa0101e4a40831ed2e2fd6a7
1001000.io <x1001000@gmail.com>
2015-11-22 07:06:55 +0000

    button

---
+++ project_3/button.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2, GPIO.IN)
+
+while True:
+  print GPIO.input(2)
+  time.sleep(0.1)
+#break the infinite loop by ctrl-c
+
+GPIO.cleanup()
