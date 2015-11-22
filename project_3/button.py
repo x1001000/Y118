fc1539c76ec6014c85f6da8420c81b76415263e5
1001000.io <x1001000@gmail.com>
2015-11-22 08:46:36 +0000

    git pull

--- project_3/button.py
+++ project_3/button.py
@@ -6,7 +6,10 @@ GPIO.setwarnings(False)
 GPIO.setup(2, GPIO.IN)
 
 while True:
-  print GPIO.input(2)
+  if GPIO.input(2):
+    print 'Oh no!!!!'
+  else:
+    print 'Thank you...'
   time.sleep(0.1)
 # break the infinite loop by ctrl-c
 
