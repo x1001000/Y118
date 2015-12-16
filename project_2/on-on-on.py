65410122ac182ebbd99f37b16ccd5d40a6482b91
x1001000 <x1001000@gmail.com>
2015-12-16 10:12:58 +0800

    GPIO.PWM(buzz, 1)

--- project_2/on-on-on.py
+++ project_2/on-on-on.py
@@ -36,16 +36,14 @@ melody = [  'G3','C4','C4','E4','A4','E4','G4',
             'F4','F4','F4','E4','A3','B3','C4','D4']
 
 note = 0
-print(push)
-'''
+
 while True:
-    while push == 0:
+    while GPIO.input(push) == False:
         print('push=0')
         time.sleep(0.001)
-    while push == 1:
+    while GPIO.input(push) == True:
         print('push=1')
         p.ChangeFrequency(freq[melody[note]])
         time.sleep(0.05)
     note += 1
     note %= 30
-'''
