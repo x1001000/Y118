de6f72f797c46b140d293144b69ecbe798e5b8d8
x1001000 <x1001000@gmail.com>
2015-12-16 12:02:22 +0800

    許願練習器

---
+++ project_3/meteor.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+row = [2,3,4,17,27,14,15,18]
+col = [26,19,13,6,5,11,9,10]
+
+for r in row:
+    GPIO.setup(r, GPIO.OUT)
+    GPIO.output(r, 0)
+
+for c in col:
+    GPIO.setup(c, GPIO.OUT)
+    GPIO.output(c, 1)
+    sleep.time(1)
