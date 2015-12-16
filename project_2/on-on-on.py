f26ba0d29e149744fe005406fcf8dd4e929b2e96
x1001000 <x1001000@gmail.com>
2015-12-16 10:40:50 +0800

    p.stop() p.start(50)

--- project_2/on-on-on.py
+++ project_2/on-on-on.py
@@ -39,10 +39,10 @@ note = 0
 while True:
     while GPIO.input(push) == False:
         p.stop()
-        time.sleep(0.001)
+        time.sleep(0.01)
     while GPIO.input(push) == True:
-        p.start(99)
-        p.ChangeFrequency(freq[melody[note]]*2)
-        time.sleep(0.05)
+        p.start(50)
+        p.ChangeFrequency(freq[melody[note]])
+        time.sleep(0.1)
     note += 1
     note %= 30
