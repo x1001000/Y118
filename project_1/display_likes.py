53cd3d7bee32cd364c360e4251fb04c4087d3a20
www.1001000.io <x1001000@gmail.com>
2016-01-05 12:28:44 +0000

    multiprocessing

--- project_1/display_likes.py
+++ project_1/display_likes.py
@@ -2,15 +2,21 @@ import RPi.GPIO as GPIO
 import time
 import urllib2
 import re
+import multiprocessing
+
+URL = 'https://www.facebook.com/YUCSA12'
+RE = '&#x80b2;&#x6210;&#x9ad8;&#x4e2d;&#x73ed;&#x806f;&#x6703;. (.),(...)'
+STRING = 'Y118'
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-segments = (10, 9, 11, 5, 6, 13, 19, 26)
+segments = [5, 9, 17, 13, 19, 11, 4, 6]
 for segment in segments:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
-digits = (3, 4, 17, 27)
+
+digits = [26, 10, 27, 22]
 for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
     GPIO.output(digit, 1)
@@ -32,26 +38,34 @@ dictionary = {
     'P':(1,1,0,0,1,1,1),
     'S':(1,0,1,0,0,1,1)}
 
-data_to_display = 'OOPS'
-
-for t in range(20):
-    page_html = urllib2.urlopen('https://www.facebook.com/dreamsat17/photos/gm.1703633339852136/967224039981992').readlines()
-    #print page_html
-    for line in page_html:
-        match = re.search('likecount..(..)', line)
-        if match:
-            data_to_display = '  ' + match.group(1)
-            print data_to_display
-            break
-    for n in range(500):
+def display(S, no_use):
+    n = 0
+    while True:
         for digit in digits:
             GPIO.output(digit, 1)
-        GPIO.output(digits[n%4], 0)
+        GPIO.output(digits[n], 0)
         i = 0
-        for on_or_off in dictionary[data_to_display[n%4]]:
+        for on_or_off in dictionary[S[n]]:
             GPIO.output(segments[i], on_or_off)
             i += 1
-        time.sleep(0.006)
-        GPIO.output(digits[n%4], 1)
+        time.sleep(0.005)
+        n += 1
+        n %= 4
+
+def likes(U, R):
+    page_source = urllib2.urlopen(U).readlines()
+    #print page_source
+    for line in page_source:
+        match = re.search(R, line)
+        if match:
+            return match.group(1) + match.group(2)
+    return 'OOPS'
+
+while True:    
+    p = multiprocessing.Process(target=display, args=(STRING, ''))
+    p.start()
+    STRING = likes(URL, RE)
+    print STRING
+    p.terminate()
 
 GPIO.cleanup()
